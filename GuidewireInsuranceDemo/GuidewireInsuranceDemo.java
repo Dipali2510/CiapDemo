6436e73673c705f60153159f394a012bd71ac47a
miteshjain <mitesh.a.jain@capgemini.com>
2020-08-27 09:29:53 +0000

    Commit timestamp : Thu Aug 27 2020 09:29:53 GMT+0000 (Coordinated Universal Time)

---
+++ GuidewireInsuranceDemo/GuidewireInsuranceDemo.java
@@ -0,0 +1,148 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import java.lang.String;
+
+public Class GuidewireInsuranceDemo {
+  
+  @Given("^User opens application <Home Page>$")
+  public void UserOpensApplicationHomePage() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User enters <User Name> and <Password>$")
+  public void UserEntersUserNameAndPassword() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^user clicks on "Log In" button$")
+  public void userClicksOnLogInButton() {
+    org.junit.Assert.fail();
+  }
+  
+  @Then("^"Landing Page" is displayed successfully$")
+  public void LandingPageIsDisplayedSuccessfully() {
+    org.junit.Assert.fail();
+  }
+  
+  @Given("^User is on "Landing Page"$")
+  public void UserIsOnLandingPage() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User clicks on "Actions " button$")
+  public void UserClicksOnActionsButton() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User clicks on "New Account" link$")
+  public void UserClicksOnNewAccountLink() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User enters <Company Name>$")
+  public void UserEntersCompanyName() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User clicks on "Search" link$")
+  public void UserClicksOnSearchLink() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User clicks on "Create New Account" link$")
+  public void UserClicksOnCreateNewAccountLink() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^user clicks on "Person" link$")
+  public void userClicksOnPersonLink() {
+    org.junit.Assert.fail();
+  }
+  
+  @Then("^"Create New Account Page" is displayed successfully$")
+  public void CreateNewAccountPageIsDisplayedSuccessfully() {
+    org.junit.Assert.fail();
+  }
+  
+  @Given("^User is on "Create New Account Page"$")
+  public void UserIsOnCreateNewAccountPage() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User enters <First name> and <Last name> and <Home Phone> and <Fax Phone> and <Primary Email> and <Work Phone>$")
+  public void UserEntersFirstNameAndLastNameAndHomePhoneAndFaxPhoneAndPrimaryEmailAndWorkPhone() {
+    org.junit.Assert.fail();
+  }
+  
+  @Given("^User is on "Create New Account Page"$")
+  public void UserIsOnCreateNewAccountPage() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User enters <Address '(.*)'  > and <City> and <County>$")
+  public void UserEntersAddress1AndCityAndCounty() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User Selects <State>$")
+  public void UserSelectsState() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User enters <ZIP Code>$")
+  public void UserEntersZIPCode() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User Selects <Address Type>$")
+  public void UserSelectsAddressType() {
+    org.junit.Assert.fail();
+  }
+  
+  @Given("^User is on "Create New Account Page"$")
+  public void UserIsOnCreateNewAccountPage() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User enters <Organization>$")
+  public void UserEntersOrganization() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User enters <Producer Code>$")
+  public void UserEntersProducerCode() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User clicks on "Update" link$")
+  public void UserClicksOnUpdateLink() {
+    org.junit.Assert.fail();
+  }
+  
+  @Then("^Account File "Summary Page" is displayed successfully$")
+  public void AccountFileSummaryPageIsDisplayedSuccessfully() {
+    org.junit.Assert.fail();
+  }
+  
+  @Given("^User is on "Summary Page"$")
+  public void UserIsOnSummaryPage() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^User clicks on "Settings" button$")
+  public void UserClicksOnSettingsButton() {
+    org.junit.Assert.fail();
+  }
+  
+  @When("^user clicks on "Logout" button$")
+  public void userClicksOnLogoutButton() {
+    org.junit.Assert.fail();
+  }
+  
+  @Then("^"Login Page" is displayed successfully$")
+  public void LoginPageIsDisplayedSuccessfully() {
+    org.junit.Assert.fail();
+  }
+  
+}
